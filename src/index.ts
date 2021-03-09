e9da94079a8c18d493ffe073742441f68adc001c
Dmitry Dudin <dima.dudin@gmail.com>
2021-03-09 11:27:38 +0700

    initial

---
+++ src/index.ts
@@ -0,0 +1,65 @@
+import { Domain, Effect } from 'effector'
+
+// Protocol
+
+export type RpcMessageId = string | number
+export type RpcMessageName = string
+export type RpcMessageParams = object
+export type RpcMessageResult = any
+
+export interface IRpcMessageError {
+  message: string
+  code?: number | null
+}
+
+export interface IRpcRequest {
+  id: RpcMessageId
+  name: RpcMessageName
+  params: RpcMessageParams
+}
+
+export interface IRpcResponse {
+  id: RpcMessageId
+  result?: RpcMessageResult
+  error?: IRpcMessageError
+}
+
+// Service
+
+export type IRpcServiceMethod = Effect<RpcMessageParams, RpcMessageResult, Error>
+
+export interface IRpcServiceMethods {
+  [key: string]: IRpcServiceMethod
+}
+
+export interface IRpcService {
+  domain: Domain
+  methods: IRpcServiceMethods
+}
+
+// Client Transport
+
+export interface IRpcClientTransport {
+  send: Effect<string, string | null>
+  open?: () => Promise<void>
+  close?: () => Promise<void>
+}
+
+// Client
+
+export interface IRpcClient<S, T> {
+  readonly service: S
+  readonly transport: T
+}
+
+export interface IRpcClientFactoryOptions<S, T> {
+  service: S
+  transport: T
+}
+
+export interface IRpcClientFactory {
+  <T extends IRpcClientTransport, S extends IRpcService>(
+    options: IRpcClientFactoryOptions<S, T>
+  ): IRpcClient<S, T>
+  of: IRpcClientFactory
+}
